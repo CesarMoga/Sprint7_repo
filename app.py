57af5e42cd05662222b64ab20f74e69f64a8b97c
CesarMoga <92492550+CesarMoga@users.noreply.github.com>
2025-05-13 23:58:10 -0600

    Agregar mensaje en app, prueba

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly_express as px
+
+st.header('Sprint 7 proyecto final vehículos')
+
+st.write('Esta aplicación aún no es funcional. En construcción')
